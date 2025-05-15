e32177f6a5e4a01be5e2a43465846a2110a28db5
sekhubede <msekhubede@gmail.com>
2025-05-15 10:20:48 +0200

    Implement excel reader functionality

--- Services/ExcelReader.cs
+++ Services/ExcelReader.cs
@@ -1,9 +1,38 @@
-﻿namespace BulkPdfCreator.Services;
+﻿using ClosedXML.Excel;
+
+namespace BulkPdfCreator.Services;
 
 public class ExcelReader(string filePath)
 {
-    public string[] ReadColumn(string columnName)
+    public List<string> ReadColumn(string columnName)
     {
-        throw new NotImplementedException();
+        var values = new List<string>();
+
+        using var workbook = new XLWorkbook(filePath);
+        var worksheet = workbook.Worksheets.First();
+        var headerRow = worksheet.Row(1);
+
+        int columnIndex = -1;
+
+        foreach (var cell in headerRow.Cells())
+        {
+            if (cell.Value.ToString().Equals(columnName, StringComparison.OrdinalIgnoreCase))
+            {
+                columnIndex = cell.Address.ColumnNumber;
+                break;
+            }
+        }
+
+        if (columnIndex == -1)
+            throw new Exception($"Column '{columnName}' not found.");
+
+        foreach (var row in worksheet.RowsUsed().Skip(1))
+        {
+            var cellValue = row.Cell(columnIndex).GetValue<string>();
+            if (!string.IsNullOrWhiteSpace(cellValue))
+                values.Add(cellValue);
+        }
+
+        return values;
     }
 }
