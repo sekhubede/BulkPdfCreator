d7a1ed854522cadd4aaa6581ace18a628957a44f
sekhubede <msekhubede@gmail.com>
2025-05-15 10:41:09 +0200

    Implement pdf generator and helper logic

--- Services/PdfGenerator.cs
+++ Services/PdfGenerator.cs
@@ -1,9 +1,32 @@
-﻿namespace BulkPdfCreator.Services;
+﻿using BulkPdfCreator.Utilities;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
 
-public class PdfGenerator(string directory, string content)
+namespace BulkPdfCreator.Services;
+
+public class PdfGenerator(string outputDirectory, string template)
 {
-   public void CreatePdf(string fileName)
+   public void CreatePdf(string rawFileName)
    {
+        var safeFileName = FileHelper.SanitizeFileName(rawFileName);
+        var filePath = Path.Combine(outputDirectory, $"{safeFileName}.pdf");
 
+        var content = template.Replace("filename", safeFileName);
+
+        Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Margin(50);
+                page.Size(PageSizes.A4);
+                page.Content().Padding(50).Column(col =>
+                {
+                    col.Item().Text("Bulk PDF Generator")
+                    .FontSize(20).Bold().FontColor(Colors.Blue.Medium);
+
+                    col.Item().Text(content).FontSize(14);
+                });
+            });
+        }).GeneratePdf(filePath);
    }
 }
--- Utilities/FileHelper.cs
+++ Utilities/FileHelper.cs
@@ -1,5 +1,15 @@
-﻿namespace BulkPdfCreator.Utilities;
+﻿using System.Text.RegularExpressions;
 
-public class FileHelper
+namespace BulkPdfCreator.Utilities;
+
+public static class FileHelper
 {
+    public static string SanitizeFileName(string name)
+    {
+        var invalidChars = Regex.Escape(new string(Path.GetInvalidFileNameChars()));
+        var regex = new Regex($"[{invalidChars}]");
+        var cleanName = regex.Replace(name, "_");
+
+        return cleanName.Length > 100 ? cleanName.Substring(0, 100) : cleanName;
+    }
 }
